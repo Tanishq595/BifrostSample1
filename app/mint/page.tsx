9eb43faae69155cd62852a19a6d324ad4e3f95d5
Tanishq Agarwal <142714429+Tanishq595@users.noreply.github.com>
2025-02-28 15:38:52 +0800

    Update page.tsx

--- app/mint/page.tsx
+++ app/mint/page.tsx
@@ -1,9 +1,5 @@
 import Navbar from "@/components/navbar"
-import Hero from "@/components/hero"
-import Features from "@/components/features"
-import CTA from "@/components/cta"
 import Footer from "@/components/footer"
-import SendTransaction from "@/components/send-transaction"; 
 import MintRedeem from "@/components/mint-redeem-lst-bifrost"
 
 export default function Home() {
